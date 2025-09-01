3164025e67ba9ce9bbc43e0ab64225f9a589c107
tiago0111 <ttiago311@gmail.com>
2025-09-01 20:51:57 +0100

    fix(client): Usa variável de ambiente para o URL da API em produção

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,11 +1,11 @@
 import Cookies from 'js-cookie';
 
-const BASE_URL = 'http://localhost:3001/api';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api';
 
 export async function fetchWithAuth(endpoint: string, options: RequestInit = {}) {
   const token = Cookies.get('authToken');
 
-  if (!token) {
+  if (!token && typeof window !== 'undefined') {
     window.location.href = '/';
     return new Response(JSON.stringify({ message: 'Não autorizado.' }), { status: 401 });
   }
@@ -21,7 +21,7 @@ export async function fetchWithAuth(endpoint: string, options: RequestInit = {})
     headers,
   });
 
-  if (response.status === 401 || response.status === 403) {
+  if ((response.status === 401 || response.status === 403) && typeof window !== 'undefined') {
     Cookies.remove('authToken');
     window.location.href = '/';
   }
